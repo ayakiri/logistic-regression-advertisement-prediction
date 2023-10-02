700a764858079cfb286bd7c8cefc1bc19939a5f7
Agata Dobrzyniewicz <s24305@pjwstk.edu.pl>
2023-10-02 17:52:22 +0200

    Import and briefly analyze data

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("ok")
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+# import
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+ad_data = pd.read_csv("advertising.csv")
+
+# analyze table
+print(ad_data.head(3))
+print(ad_data.info())
+print(ad_data.describe())
+
+# analyze data
+sns.pairplot(ad_data, hue="Clicked on Ad", palette="husl")
+plt.show()
+
+sns.histplot(ad_data, x="Age", bins=40)
+sns.jointplot(ad_data, x="Age", y="Area Income")
+sns.jointplot(ad_data, x="Age", y="Daily Time Spent on Site", kind="kde")
+plt.show()
